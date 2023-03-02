82c25f6e34171152825910aa88edb1f2c0245641
Алёна <aaaaaalena00@mail.ru>
2023-03-02 11:14:43 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+'''
+Title
+Group:
+...
+////
+'''
+def triangle(x,y,a,b, angle, ):
+    '''
+    Function for drawing triangle.
+    :param x:
+    :param y:
+    :param a:
+    :param b:
+    :param angle:
+    :return: none
+    '''
+    pass
+def draw_dog():
+    pass
+def draw_cat():
+    pass
+def main():
+    '''
+    Main function:
+    :return:
+    '''
+    draw_cat()
+    draw_dog()
+
+if __name__ == '__main__':
+    main()
